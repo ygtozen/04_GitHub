d32c0491069e20f70651e398796395cbdafdaf01
ygtozen <ozenyigit151@gmail.com>
2023-11-11 19:10:12 +0300

    Bu benim ilk gönderim.

---
+++ 04_GitHub/src/main/java/com/ozen/_GitHub/App.java
@@ -0,0 +1,22 @@
+package com.ozen._GitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Islem islem = new Islem();
+    	islem.goster();
+    	      
+        
+        selamla();
+    }
+
+	private static void selamla() {
+		System.out.println( "Selam Github!" );
+		
+	}
+}
---
+++ 04_GitHub/src/main/java/com/ozen/_GitHub/Islem.java
@@ -0,0 +1,11 @@
+package com.ozen._GitHub;
+
+public class Islem {
+	
+	int veriler [] = {1,2,3};
+	
+	public void goster() {
+		System.out.println("Sonuçları göster.");
+	}
+
+}
